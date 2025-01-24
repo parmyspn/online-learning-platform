5ea8fa171d1ba40643eda839e249babb0e3851b1
parmyspn <parmisprv@gmail.com>
2025-01-24 01:09:54 -0800

    db migrate updates

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,12 +3,7 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-bundle exec rails db:migrate
-
-# If you're using a Free instance type, you need to
-# perform database migrations in the build command.
-# Uncomment the following line:
-
-# bundle exec rails db:migrate
+./bin/rails assets:precompile
+./bin/rails assets:clean
+./bin/rails db:migrate
+./bin/rails db:seed
